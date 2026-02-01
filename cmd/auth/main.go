6625d67c7b6cb80f65836bf945f1ecc42983214a
pavel <pavelrz648@yandex.ru>
2026-02-01 16:28:46 +0300

    slog logger init

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "github.com/meis1kqt/sso/internal/config"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/meis1kqt/sso/internal/config"
+)
 
 func main() {
 
-	Config := config.MustLoad()
+	cfg := config.MustLoad()
+	
+	var logger *slog.Logger
 
-	_ = Config
+	logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	slog.SetDefault(logger)
 
 }
