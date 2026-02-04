780a1b9b4222b3c938941c085d0c8e5a1af6fc84
pavel <pavelrz648@yandex.ru>
2026-02-04 09:37:28 +0300

    services sloy init

--- go.mod
+++ go.mod
@@ -11,15 +11,17 @@ require (
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/ncruces/go-strftime v1.0.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	golang.org/x/crypto v0.47.0 // indirect
 	golang.org/x/exp v0.0.0-20251023183803-a4bb9ffd2546 // indirect
-	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/sys v0.38.0 // indirect
-	golang.org/x/text v0.31.0 // indirect
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ internal/services/auth/auth.go
@@ -0,0 +1,98 @@
+package auth
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/meis1kqt/sso/internal/domain/models"
+	"github.com/meis1kqt/sso/internal/lib/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Auth struct {
+	log      *slog.Logger
+	storage  Storage
+	tokenTTL time.Duration
+}
+
+type Storage interface {
+	SaveUser(ctx context.Context, email string, passHash []byte) (userID int64, err error)
+	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+	App(ctx context.Context, appID int64) (models.App, error)
+}
+
+func New(log *slog.Logger, storage *Storage, tokenTTL time.Duration) *Auth {
+	return &Auth{
+		log:      log,
+		storage:  *storage,
+		tokenTTL: tokenTTL,
+	}
+}
+
+func (a *Auth) Login(ctx context.Context, email string, password string, appID int64) (string, error) {
+
+	user, err := a.storage.User(ctx, email)
+
+	if err != nil {
+		slog.Error("failed to get User", "error", err)
+		return "", err
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.PassHash, []byte(password)); err != nil {
+		slog.Error("invalid password", "error", err)
+		return "", err
+	}
+
+	app, err := a.storage.App(ctx, appID)
+	if err != nil {
+		slog.Error("invalid app", "error", err)
+		return "", err
+	}
+
+	slog.Info("user is logged in successfully")
+
+	token, err := jwt.NewToken(user, app, a.tokenTTL)
+
+	if err != nil {
+		slog.Error("token error", "error", err)
+		return "", err
+	}
+
+	return token, nil
+}
+
+func (a *Auth) RegisterNewUser(ctx context.Context, email string, password string) (int64, error) {
+	slog.Info("registering user")
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+
+	if err != nil {
+		slog.Error("password hash error", "error", err)
+		return 0, err
+	}
+
+	id, err := a.storage.SaveUser(ctx, email, passHash)
+
+	if err != nil {
+		slog.Error("saveuser", "error", err)
+		return 0, err
+	}
+
+	slog.Info("user registered")
+
+	return id, nil
+
+}
+
+func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	isAdmin, err := a.storage.IsAdmin(ctx, userID)
+
+	if err != nil {
+		slog.Error("bag", "error", err)
+		return false, err
+	}
+
+	return isAdmin, nil
+}
